b38e4887d912c30721f23e2945e30ebb387e2a79
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-06-06 16:28:03 -0700

    use router

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from 'express';
+import fs from 'fs';
+
 
 import config from './config';
+import apiRouter from './api';
+
 
 const server = express();
+
+// middle ware for static file
+server.use(express.static('public'));
+
+// Use router as middle ware
+server.use('/api', apiRouter);
 
 server.get('/', (req, res) => {
     res.send('Hello Express');
 })
-
-server.get('/about.html', (req, res) => {
-    res.send('The about page.');
-});
 
 server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
